d1d901722859934da37a7aca48ae9888059a37af
debashish0305 <57298944+debashish0305@users.noreply.github.com>
2024-04-12 00:25:33 +0530

    Update fetchFromDBAddToHashMap.sh

--- src/main/fetchFromDBAddToHashMap.sh
+++ src/main/fetchFromDBAddToHashMap.sh
@@ -71,3 +71,35 @@ echo "$output" | while IFS=$'\t' read -r col1 col2 col3 col4 col5 col6 col7; do
     # Print each row
     echo "Row: $col1, $col2, $col3, $col4, $col5, $col6, $col7"
 done
+############################
+#!/bin/bash
+
+# Database connection details
+DB_USER="your_db_username"
+DB_PASS="your_db_password"
+DB_NAME="your_db_name"
+DB_HOST="your_db_host"
+
+# Input parameters (three columns)
+COLUMN1="$1"
+COLUMN2="$2"
+COLUMN3="$3"
+
+# SQL query to fetch data
+query="SELECT column1, column2, column3, column4, column5, column6, column7 FROM your_table WHERE column1='$COLUMN1' AND column2='$COLUMN2' AND column3='$COLUMN3';"
+
+echo "SQL Query: $query"
+
+# Connect to the database and execute the query
+output=$(sqlplus -S "$DB_USER/$DB_PASS@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$DB_HOST)(PORT=1521))(CONNECT_DATA=(SID=$DB_NAME)))" <<EOF
+set pagesize 0
+set feedback off
+set verify off
+set heading off
+set echo off
+$query
+EOF
+)
+
+echo "Output from SQL*Plus:"
+echo "$output"
